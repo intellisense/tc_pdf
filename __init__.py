e1654f34c3568fec642234468a886c6960c8d5a0
Aamir Adnan <aamir.adnan.rind@gmail.com>
2017-12-15 01:35:32 +0500

    remove root __init__

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from tc_core import Extension, Extensions
-from tc_pdf.handlers.pdf import PDFHandler
-
-extension = Extension('tc_pdf')
-
-# Register the route
-extension.add_handler(PDFHandler.regex(), PDFHandler)
-
-Extensions.register(extension)
