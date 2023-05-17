8f6fd976a9fc11a7b627a8e28e9d62ac12541ede
Aamir Rind <aamir.adnan.rind@gmail.com>
2023-05-17 05:14:51 +0500

    Bump version to 1.0.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tc_pdf',
-    version='0.0.7',
+    version='1.0.0',
     url='http://github.com/intellisense/tc_pdf',
     license='MIT',
     author='Aamir Rind',
